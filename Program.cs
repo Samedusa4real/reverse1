f0e842b65a83f0bebd1075d0d58fdf9d923d8ce5
Samedusaa <semed.musayev.2003@mail.ru>
2022-12-14 00:35:40 +0400

    new task done

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string name = "C# is programming language";
+string name1 = "";
+
+
+for (int i = name.Length - 1; i >= 0; i--)
+{
+    name1 += name[i].ToString();
+}
+
+string[] newName1;
+newName1 = name1.Split(' ');
+
+for (int i = newName1.Length - 1; i >= 0; i--)
+{
+    Console.Write(newName1[i]+" ");
+}
+
+
+
+//string[] newName;
+
+//newName = name.Split(' ');
+
+//Console.WriteLine(newName[0]);
